eb18747aec5235daef78e94cae5a7ccc3f27f4d0
Stephen Schaub <sschaub@gmail.com>
2020-05-27 15:44:17 -0400

    Add chickenfeed

--- Lesson 2-1 Software Development/chickenfeed_buggy.py
+++ Lesson 2-1 Software Development/chickenfeed_buggy.py
@@ -1,12 +1,12 @@
-# The Chicken Feed Calculator
+# The Chicken Feed Calculator (buggy version)
 
 current_temp = int(input('Enter the temperature in degrees fahrenheit: '))
 total_weight = int(input('Enter total chicken weight in pounds:'))
 
-if current_temp < 60:
+if current_temp <= 60:
     print("It's cold! temperature =", current_temp)   # for debugging
     total_feed = total_weight * .12
-elif current_temp > 90:
+elif current_temp >= 90:
     print("It's hot! temperature =", current_temp)    # for debugging
     total_feed = total_weight * .08
 else:
